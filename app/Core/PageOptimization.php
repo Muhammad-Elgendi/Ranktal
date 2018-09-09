3b3e876e65c3bce7510f42fad9dc473f5afd0aa8
Muhammad Elgendi <actnoon@gmail.com>
2018-09-09 21:41:22 +0200

    init page optimization

--- app/Core/PageOptimization.php
+++ app/Core/PageOptimization.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Core;
+require 'vendor/autoload.php';
 
 /**
  *
@@ -10,5 +11,76 @@ namespace App\Core;
  **/
 
  class PageOptimization{
+
+    function __constructor(){
+
+    }
+
+    private function makeConnection($url){
+        // Use Curl to send off your request.
+        $options = array(
+            CURLOPT_RETURNTRANSFER => true
+        );
+        $ch = curl_init($url);
+        curl_setopt_array($ch, $options);
+        $content = curl_exec($ch);
+        curl_close($ch);
+        if ( $content === false )
+            return false;
+        else
+            return $content;
+    }
+
+    private function isAllowedFromRobots($url){
+        $robots=parse_url($url, PHP_URL_SCHEME).'://'.parse_url($url, PHP_URL_HOST).'/robots.txt';
+		$content=$this->makeConnection($robots);
+        if($content) {
+            $parser = new RobotsTxtParser($content);
+            $agents=['*','Googlebot','Bingbot','Slurp','DuckDuckBot','Baiduspider','YandexBot'];            
+            foreach($agents as $agent){
+                $parser->setUserAgent($agent);         
+                if ($parser->isDisallowed(parse_url($url, PHP_URL_PATH))) {
+                    return false;
+                }
+            }
+            return true;            
+        }else{
+            return true;
+        }
+    }
+
+    private function get_headers_from_curl_response($response){
+    $headers = array();
+
+    $header_text = substr($response, 0, strpos($response, "\r\n\r\n"));
+
+    foreach (explode("\r\n", $header_text) as $i => $line)
+        if ($i === 0)
+            $headers['http_code'] = $line;
+        else
+        {
+            list ($key, $value) = explode(': ', $line);
+
+            $headers[$key] = $value;
+        }
+
+    return $headers;
+    }
+
+    private function isAllowedFromPage(){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        // ...
+        
+        $response = curl_exec($ch);
+        
+        // Then, after your curl_exec call:
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $header = substr($response, 0, $header_size);
+        $body = substr($response, $header_size);
+    }
      
  }
