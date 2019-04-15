c93406f06589d743aabee0771be519d19aaa2f0e
Muhammad Elgendi <actnoon@gmail.com>
2019-04-15 21:17:50 +0200

    Finish Page Optimization tool

--- resources/views/dashboard/pageOptimization.blade.php
+++ resources/views/dashboard/pageOptimization.blade.php
@@ -3,6 +3,7 @@
 @section('title', __('page-optimization'))
 @section('user-image',url('/img/user.png'))
 @section('user-type',__('pro-plan'))
+@section('print-div','printable')
 @section('styles')
 <!-- Styles -->
 <style>
@@ -36,79 +37,82 @@
 @endsection
 
 @section('content')
-<div class="container-fluid">    
+<div class="container-fluid" id="printable">    
         <div class="row">
-                {!! Form::open(['url' => 'report']) !!}
-                <div class="col-lg-5 col-xs-12">
+                {!! Form::open(['url' => 'report','id'=>'the-form']) !!}
+                <div class="col-lg-5 col-md-5 col-sm-6 col-xs-12">
                                 <p class="title">@lang('page-link')</p>
                                 <div class="form-group" id="text-box">
-                                    {{ Form::text('url', null, ['class' => 'form-control','placeholder'=>__('page-link') ,'id'=>'urlInput']) }}
+                                    {{ Form::text('url', null, ['class' => 'form-control','placeholder'=>__('page-link') ,'id'=>'urlInput','pattern'=>'https?://.+','required','title'=>'Page Link begins with http:// Or https://']) }}
                                 </div>
                 </div>
-                <div class="col-lg-5 col-xs-12">
+                <div class="col-lg-5 col-md-5 col-sm-6 col-xs-12">
                                 <p class="title">@lang('keyword')</p>
                                 <div class="form-group" id="text-box">
-                                    {{ Form::text('url', null, ['class' => 'form-control','placeholder'=>__('keyword') ,'id'=>'keywordInput']) }}
+                                    {{ Form::text('url', null, ['class' => 'form-control','placeholder'=>__('keyword') ,'id'=>'keywordInput','pattern'=>'.{1,}','required','title'=>'Enter your Keyword']) }}
                                 </div>                            
                 </div>
-                <div class="col-lg-2 col-xs-12">
-                           <button id="button" type="button" class="btn btn-primary form-control" style="margin-top: 38px;"><i class="fa fa-search"></i></button>
+                <div class="col-lg-2 col-md-2 col-sm-3 col-xs-12">
+                           <button id="button" type="button" class="btn btn-primary form-control hidden-print" style="margin-top: 38px;"><i class="fa fa-search"></i></button>
+                           <input type="submit" style="display:none;"/>
                 </div>
                 {!! Form::close() !!}
         </div>
 
-        <div style="display:none; padding-bottom: 15px;" id="upper-board">
+        
 
-            <div class="row">    
-                <div class="col-lg-6 col-xs-12" id="title">
-                        <p class="title">@lang('page-link')</p> 
-                        <a id="url-value" href=""></a>
-                        
-                        <p class="title">@lang('page-title')</p> 
-                        <p id="title-value"></p>
+            <div style="display:none; padding-bottom: 15px;" id="upper-board">
+
+                <div class="row">    
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" id="title">
+                            <p class="title">@lang('page-link')</p> 
+                            <a id="url-value" href=""></a>
+                            
+                            <p class="title">@lang('page-title')</p> 
+                            <p id="title-value"></p>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" id="desc">
+                            <p class="title">@lang('page-description')</p> 
+                            <p id="desc-value"></p> 
+                    </div>
                 </div>
 
-                <div class="col-lg-6 col-xs-12" id="desc">
-                        <p class="title">@lang('page-description')</p> 
-                        <p id="desc-value"></p> 
+                <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12" >
+                        <p class="title">@lang('keyword-found')</p> 
+                        <p class="title" id="found"></p> 
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                        <p class="title">@lang('keyword-locations')</p> 
+                        <ul id="locations"></ul> 
+                    </div>             
+                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6" id="page-score">
+
+                        <p class="title">@lang('page-score')</p>
+                        <div id="process-bar"></div>
+
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 container-border text-center" id="hurting-block">
+                        <p class="title">@lang('hurt-score') <span class="glyphicon glyphicon-chevron-down" style="color:#ff4444;"></span></p>
+                        <p class="title" id="hurting"></p>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 container-border text-center" id="helping-block">
+                        <p class="title">@lang('help-score') <span class="glyphicon glyphicon-chevron-up" style="color:#00C851;"></span></p>
+                        <p class="title" id="helping"></p>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 container-border text-center" id="issues-block">
+                        <p class="title">@lang('all-issues')</p>
+                        <p class="title"  id="all-issues"></p>
+                    </div>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-lg-3 col-xs-12" >
-                    <p class="title">@lang('keyword-found')</p> 
-                    <p class="title" id="found"></p> 
-                </div>
-                <div class="col-lg-3 col-xs-12">
-                    <p class="title">@lang('keyword-locations')</p> 
-                    <ul id="locations"></ul> 
-                </div>             
-                <div class="col-lg-3 col-xs-12" id="page-score">
-
-                    <p class="title">@lang('page-score')</p>
-                    <div id="process-bar"></div>
-
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-lg-3 col-xs-12 container-border text-center" id="hurting-block">
-                    <p class="title">@lang('hurt-score')</p>
-                    <p class="title" id="hurting"></p>
-                </div>
-                <div class="col-lg-3 col-xs-12 container-border text-center" id="helping-block">
-                    <p class="title">@lang('help-score')</p>
-                    <p class="title" id="helping"></p>
-                </div>
-                <div class="col-lg-3 col-xs-12 container-border text-center" id="issues-block">
-                    <p class="title">@lang('all-issues')</p>
-                    <p class="title"  id="all-issues"></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row" id="checks"></div>
-
+            <div class="row" id="checks"></div>
+    
 </div>
 
 
@@ -121,21 +125,36 @@
 
 <script>
   $( "#button" ).click(function() {
-    
-    // reset process bar
-    $('#process-bar').html("");
+    if($("#the-form")[0].checkValidity()) {
+  
+        //disable button
+        $("#button").attr("disabled", true);
 
-    $.get("{{route('lang.optimizerAjax',app()->getLocale())}}",{ u: $("#urlInput").val(), k: $("#keywordInput").val() }, function (jsondata) {
+        // reset process bar,checks
+        $('#checks,#process-bar').html("");
 
-            $.get("{{url('templates/panelComponent.hbs')}}", function (data) {
-            var template=Handlebars.compile(data);      
-            $("#checks").html(template(jsondata.checks));
-                // update view
-                updateView(jsondata);
-                attachProcessPar(jsondata);
-            }, 'html')
+        $.get("{{route('lang.optimizerAjax',app()->getLocale())}}",{ u: $("#urlInput").val(), k: $("#keywordInput").val() }, function (jsondata) {
+            $("#urlInput").attr("placeholder", $("#urlInput").val());
+            $("#keywordInput").attr("placeholder", $("#keywordInput").val());
 
+                $.get("{{url('templates/panelComponent.hbs')}}", function (data) {
+                var template=Handlebars.compile(data);      
+                $("#checks").html(template(jsondata.checks));
+                    // update view
+                    updateView(jsondata);
+                    attachProcessPar(jsondata);
+                }, 'html')       
         },'json')
+
+        //Enable button
+        setTimeout(function() {
+            $("#button").removeAttr("disabled");
+        },2500);   // enable after 2 seconds       
+        
+    }
+    else {
+        $("#the-form")[0].reportValidity();
+    }
 });
 
 function updateView(jsondata){
